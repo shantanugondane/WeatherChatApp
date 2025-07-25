ca47514d320080b2b0c79cc60a79aac7bbe9beb9
shantanugondane <shantanugondane44@gmail.com>
2025-07-25 13:04:53 +0530

    done with the change

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -9,7 +9,11 @@ export default function ChatMessage({ message }) {
     <div className={`message-container ${isUser ? 'message-user' : 'message-assistant'}`}>
       <div className={`message-bubble ${isUser ? 'user' : message.isError ? 'error' : 'assistant'}`}>
         <div className="whitespace-pre-wrap break-words">
-          {message.content}
+          {isUser ? (
+            message.content
+          ) : (
+            <span dangerouslySetInnerHTML={{ __html: message.content }} />
+          )}
         </div>
         <div className="text-xs opacity-70 mt-2">
           {timestamp}
